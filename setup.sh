0bacdfb5bc5661b66f7ce042ddfd6d3de6f2d2f5
rosicenter691 <rosicenter691@gmail.com>
2025-04-18 13:04:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ gem install lolcat
 apt install wondershaper -y
 clear
 # REPO    
-    REPO="https://raw.githubusercontent.com/Panwas89/st-pusat/main/"
+    REPO="https://raw.githubusercontent.com/rosicenter691/st-pusat/main/"
 
 ####
 start=$(date +%s)
